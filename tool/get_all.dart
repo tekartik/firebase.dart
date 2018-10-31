638ef0ba67dba48caf78da4ec50948d97006d99c
Alexandre Roux <alex@tekartik.com>
2018-10-31 16:55:40 +0100

    fix: dart tools

--- tool/get_all.dart
+++ tool/get_all.dart
@@ -9,76 +9,15 @@ Future getAll(List<String> packages) async {
   await Future.wait(futures);
 }
 
-Future testFirebase() async {
-  var dir = 'firebase';
-  await runCmd(PubCmd(pubGetArgs())..workingDirectory = dir);
-  await runCmd(DartAnalyzerCmd(['lib', 'test'])..workingDirectory = dir);
-  await runCmd(PubCmd(pubRunTestArgs(platforms: ['vm', 'chrome']))
-    ..workingDirectory = dir);
-}
-
-Future testFirebaseSembast() async {
-  var dir = 'firebase_sembast';
-  await runCmd(PubCmd(pubGetArgs())..workingDirectory = dir);
-  await runCmd(DartAnalyzerCmd(['lib', 'test'])..workingDirectory = dir);
-  await runCmd(
-      PubCmd(pubRunTestArgs(platforms: ['vm']))..workingDirectory = dir);
-}
-
-Future testFirebaseSim() async {
-  var dir = 'firebase_sim';
-  await runCmd(PubCmd(pubGetArgs())..workingDirectory = dir);
-  await runCmd(DartAnalyzerCmd(['lib', 'test'])..workingDirectory = dir);
-  await runCmd(PubCmd(pubRunTestArgs(platforms: ['vm', 'chrome']))
-    ..workingDirectory = dir);
-}
-
-Future testFirebaseSimIo() async {
-  var dir = 'firebase_sim_io';
-  await runCmd(PubCmd(pubGetArgs())..workingDirectory = dir);
-  await runCmd(DartAnalyzerCmd(['lib', 'test'])..workingDirectory = dir);
-  await runCmd(
-      PubCmd(pubRunTestArgs(platforms: ['vm']))..workingDirectory = dir);
-}
-
-Future testFirebaseBrowser() async {
-  var dir = 'firebase_browser';
-  await runCmd(PubCmd(pubGetArgs())..workingDirectory = dir);
-  await runCmd(DartAnalyzerCmd(['lib', 'test'])..workingDirectory = dir);
-  /*
-  await runCmd(
-      PubCmd(pubRunTestArgs(platforms: ['chrome']))..workingDirectory = dir);
-      */
-}
-
-Future testFirebaseSimBrowser() async {
-  var dir = 'firebase_sim_browser';
-  await runCmd(PubCmd(pubGetArgs())..workingDirectory = dir);
-  await runCmd(DartAnalyzerCmd(['lib', 'test'])..workingDirectory = dir);
-  /*
-  await runCmd(
-      PubCmd(pubRunTestArgs(platforms: ['chrome']))..workingDirectory = dir);
-      */
-}
-
-Future testFirebaseNode() async {
-  var dir = 'firebase_node';
-  await runCmd(PubCmd(pubGetArgs())..workingDirectory = dir);
-  await runCmd(DartAnalyzerCmd(['lib', 'test'])..workingDirectory = dir);
-  await runCmd(
-      PubCmd(pubRunTestArgs(platforms: ['node']))..workingDirectory = dir);
-}
-
-Future testFirebaseFlutter() async {
-  var dir = 'firebase_flutter';
-  await runCmd(FlutterCmd(['packages', 'get'])..workingDirectory = dir);
-  await runCmd(DartAnalyzerCmd(['lib'])..workingDirectory = dir);
-}
-
-Future testFirebaseTest() async {
-  var dir = 'firebase_test';
-  await runCmd(PubCmd(pubGetArgs())..workingDirectory = dir);
-  await runCmd(DartAnalyzerCmd(['lib'])..workingDirectory = dir);
+Future flutterGetAll(List<String> packages) async {
+  if (isFlutterSupported) {
+    var futures = <Future>[];
+    for (var package in packages) {
+      await runCmd(FlutterCmd(['packages', 'get'])
+        ..workingDirectory = package);
+    }
+    await Future.wait(futures);
+  }
 }
 
 var packages = [
@@ -86,20 +25,13 @@ var packages = [
   'firebase_browser',
   'firebase_sembast',
   'firebase_sim',
+  'firebase_test',
   'firebase_sim_io',
   'firebase_sim_browser'
 ];
+
+var flutterPackages = ['firebase_flutter'];
+
 Future main() async {
-  await getAll(packages);
-  /*
-  await testFirebase();
-  await testFirebaseBrowser();
-  await testFirebaseSembast();
-  await testFirebaseSim();
-  await testFirebaseSimBrowser();
-  await testFirebaseSimIo();
-  await testFirebaseNode();
-  await testFirebaseFlutter();
-  await testFirebaseTest();
-  */
+  await Future.wait([getAll(packages), flutterGetAll(flutterPackages)]);
 }
