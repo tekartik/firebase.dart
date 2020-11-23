24cd5e12a56737809be356c1dc230c1b8538ddd9
alex <alex@tekartik.com>
2020-11-23 19:03:19 +0100

    fix: firebase node setup

--- firebase_node/test/firebase_node_test.dart
+++ firebase_node/test/firebase_node_test.dart
@@ -1,11 +1,13 @@
 @TestOn('node')
 library tekartik_firebase_server_node.node_test;
 
+import 'package:tekartik_app_node_utils/node_utils.dart';
 import 'package:tekartik_firebase_node/firebase_node.dart';
 import 'package:test/test.dart';
 
+var _env = platform.environment;
 // GOOGLE_APPLICATION_CREDENTIALS must be defined in an environment variable
-// pointing to the relevan json path
+// pointing to the relevant json path
 void main() {
   /*
   group('node', () {
@@ -14,11 +16,17 @@ void main() {
   });
    */
   group('firebase admin', () {
+    test('app', () {
+      print('FIREBASE_CONFIG: ${_env['FIREBASE_CONFIG']}');
+      print(
+          'GOOGLE_APPLICATION_CREDENTIALS: ${_env['GOOGLE_APPLICATION_CREDENTIALS']}');
+    });
     test('access token', () async {
-      var app = firebaseNode.initializeApp(name: 'admin');
+      var app = await firebaseNode.initializeAppAsync(name: 'admin');
       print(
           (await firebaseNode.credential.applicationDefault().getAccessToken())
               .data);
+      print(app.options);
       print(app.options.projectId);
       await app.delete();
     });
