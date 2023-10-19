8b6324ee9e743dc1c7aff4d49943c5931459b9d6
Alexandre Roux <alex@tekartik.com>
2023-10-19 11:59:30 +0200

    fix ci

--- firebase_rest/lib/src/firebase_rest_web.dart
+++ firebase_rest/lib/src/firebase_rest_web.dart
@@ -3,3 +3,6 @@ import 'package:tekartik_firebase_rest/firebase_rest.dart';
 FirebaseAdminCredentialRest newFromServiceAccountJson(String serviceAccountJson,
         {List<String>? scopes}) =>
     throw UnsupportedError('newFromServiceAccountJson');
+FirebaseAdminCredentialRest newFromServiceAccountMap(Map serviceAccountMap,
+        {List<String>? scopes}) =>
+    throw UnsupportedError('newFromServiceAccountJson');
